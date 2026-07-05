ec08059193349d6dae38a488dc0fdc31ab6b81b6
Karthik Chowdary <karthikchowdary2410@gmail.com>
2026-07-05 22:36:57 +0530

    Fix upload folder creation on Render

--- app.py
+++ app.py
@@ -75,6 +75,7 @@ groq_client = Groq(
 UPLOAD_FOLDER = "static/uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
+os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 # Home Page
 @app.route("/")
